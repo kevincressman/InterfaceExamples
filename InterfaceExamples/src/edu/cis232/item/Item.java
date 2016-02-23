fa390522bc801f724e780d72f89d927ec0f56795
SK207_inst <SK207_inst@K207_W8Inst.carrollcc.edu>
2016-02-22 19:49:51 -0500

    First commit

---
+++ InterfaceExamples/src/edu/cis232/item/Item.java
@@ -0,0 +1,17 @@
+package edu.cis232.item;
+
+public abstract class Item {
+	private String name;
+	
+	public Item(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+}
